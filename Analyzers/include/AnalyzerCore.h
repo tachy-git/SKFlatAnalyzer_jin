c0535fb50eea04a34ece3b65204f43c74a8bb755
shjeon <shjeon@cern.ch>
2022-07-18 07:07:05 +0900

    Revert "Add HEM veto electron option"
    This reverts commit bcb2a7b59786289dd2aa0a9242fd6dc1bfc70016.

--- Analyzers/include/AnalyzerCore.h
+++ Analyzers/include/AnalyzerCore.h
@@ -65,7 +65,7 @@ public:
 
 
   std::vector<Electron> GetAllElectrons();
-  std::vector<Electron> GetElectrons(TString id, double ptmin, double fetamax, bool vetoHEM = false);
+  std::vector<Electron> GetElectrons(TString id, double ptmin, double fetamax);
 
   std::vector<Muon> GetAllMuons();
   std::vector<Muon> GetMuons(TString id, double ptmin, double fetamax);
@@ -201,7 +201,6 @@ public:
   static bool IsFinalPhotonSt23_Public(const std::vector<Gen>& TruthColl);
   int  GetPrElType_InSameSCRange_Public(int TruthIdx, const std::vector<Gen>& TruthColl);
   bool IsSignalPID(int pid);
-  bool FindHEMElectron(Electron electron);
 
   //==== Plotting
 
