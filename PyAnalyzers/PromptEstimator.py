000b60eb69e538089e339c996b90cc5b16e4f6fa
choijin <jin.choi@cern.ch>
2023-02-03 10:29:27 +0900

    modify SKFlat.py: initializeAnalyzer -> initializePyAnalyzer

--- PyAnalyzers/PromptEstimator.py
+++ PyAnalyzers/PromptEstimator.py
@@ -602,26 +602,3 @@ class PromptEstimator(TriLeptonBase):
         with torch.no_grad():
             out = self.models[modelKey](data.x, data.edge_index)
         return out.numpy()[0][1]
-
-if __name__ == "__main__":
-    m = PromptEstimator()
-    m.SetTreeName("recoTree/SKFlat")
-    m.IsDATA = False
-    m.MCSample = "TTToHcToWAToMuMu_MHc-130_MA-90"
-    m.xsec = 0.015
-    m.sumSign = 599702.0
-    m.sumW = 3270.46
-    m.IsFastSim = False
-    m.SetEra("2017")
-    m.Userflags = std.vector[std.string]()
-    m.Userflags.emplace_back("Skim3Mu")
-    m.Userflags.emplace_back("DenseNet")
-    if not m.AddFile("/home/choij/workspace/DATA/SKFlat/Run2UltraLegacy_v3/2017/TTToHcToWAToMuMu_MHc-130_MA-90_MultiLepFilter_TuneCP5_13TeV-madgraph-pythia8/SKFlat_Run2UltraLegacy_v3/220714_084244/0000/SKFlatNtuple_2017_MC_1.root"): exit(1)
-    #if not m.AddFile("/home/choij/workspace/DATA/SKFlat/Run2UltraLegacy_v3/2017/TTToHcToWAToMuMu_MHc-130_MA-90_MultiLepFilter_TuneCP5_13TeV-madgraph-pythia8/SKFlat_Run2UltraLegacy_v3/220714_084244/0000/SKFlatNtuple_2017_MC_8.root"): exit(1)
-    m.SetOutfilePath("histsDense.root")
-    m.Init()
-    m.initializePyAnalyzer()
-    m.initializeAnalyzerTools()
-    m.SwitchToTempDir()
-    m.Loop()
-    m.WriteHist()
