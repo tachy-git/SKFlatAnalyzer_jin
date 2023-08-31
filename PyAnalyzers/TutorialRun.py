32abc7825adbd337a0b1e44d514415c43742c65a
Jin Choi <choij@cern.ch>
2023-08-31 19:30:30 +0900

    update TutorialRun

--- PyAnalyzers/TutorialRun.py
+++ PyAnalyzers/TutorialRun.py
@@ -7,6 +7,9 @@ class TutorialRun(TutorialBase):
     def __init__(self):
         super().__init__()
 
+    def intializePyAnalyzer(self):
+        super().initializeAnalyzer()
+
     # Override executeEvent function
     def executeEvent(self):
         allMuons = super().GetAllMuons()
@@ -62,23 +65,3 @@ class TutorialRun(TutorialBase):
             super().FillHist(f"{muonID}/ZCand/phi", ZCand.Phi(), weight, 64, -3.2, 3.2);
             super().FillHist(f"{muonID}/MET", METv.Pt(), weight, 300, 0., 300.);
             super().FillHist(f"{muonID}/bjets/size", len(bjets), weight, 20, 0., 20.);
-
-if __name__ == "__main__":
-    m = TutorialRun()
-    m.SetTreeName("recoTree/SKFlat")
-    m.IsDATA = False
-    m.MCSample = "TTToHcToWAToMuMu_MHc-130_MA-90"
-    m.xsec = 0.015
-    m.sumSign = 599702.0
-    m.sumW = 3270.46
-    m.IsFastSim = False
-    m.SetEra("2017")
-    if not m.AddFile("/home/choij/workspace/DATA/SKFlat/Run2UltraLegacy_v3/2017/TTToHcToWAToMuMu_MHc-130_MA-90_MultiLepFilter_TuneCP5_13TeV-madgraph-pythia8/SKFlat_Run2UltraLegacy_v3/220714_084244/0000/SKFlatNtuple_2017_MC_14.root"): exit(1)
-    if not m.AddFile("/home/choij/workspace/DATA/SKFlat/Run2UltraLegacy_v3/2017/TTToHcToWAToMuMu_MHc-130_MA-90_MultiLepFilter_TuneCP5_13TeV-madgraph-pythia8/SKFlat_Run2UltraLegacy_v3/220714_084244/0000/SKFlatNtuple_2017_MC_5.root"): exit(1)
-    m.SetOutfilePath("hists.root")
-    m.Init()
-    m.initializeAnalyzer()
-    m.initializeAnalyzerTools()
-    m.SwitchToTempDir()
-    m.Loop()
-    m.WriteHist()
