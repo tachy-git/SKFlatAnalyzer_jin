97ef0e2df4a5e4b00f21e39985f8392c3d2047a7
Jin Choi <choij@cern.ch>
2023-08-31 19:37:00 +0900

    update TutorialRun

--- PyAnalyzers/TutorialRun.py
+++ PyAnalyzers/TutorialRun.py
@@ -7,7 +7,7 @@ class TutorialRun(TutorialBase):
     def __init__(self):
         super().__init__()
 
-    def intializePyAnalyzer(self):
+    def initializePyAnalyzer(self):
         super().initializeAnalyzer()
 
     # Override executeEvent function
@@ -56,8 +56,6 @@ class TutorialRun(TutorialBase):
                 # Muon ID efficiency
                 #for mu in muons:
                 #    print(super().mcCorr.MuonID_SF("NUM_TopHN_DEN_TrackerMuons", mu.Eta(), mu.Pt()))
-
-
 
             super().FillHist(f"{muonID}/ZCand/mass", ZCand.M(), weight, 40, 70., 110.);
             super().FillHist(f"{muonID}/ZCand/pt", ZCand.Pt(), weight, 300, 0., 300.);
