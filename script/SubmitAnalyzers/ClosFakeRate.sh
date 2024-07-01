c5245aafb5b39a0d5c7c98e08605e49a2a1270e6
Jin Choi <jin.choi@cern.ch>
2024-07-01 16:38:37 +0900

    sync update

---
+++ script/SubmitAnalyzers/ClosFakeRate.sh
@@ -0,0 +1,8 @@
+SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 20 -e 2016preVFP --userflags Skim1E2Mu,FakeStudy --python &
+SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 20 -e 2016preVFP --userflags Skim3Mu,FakeStudy --python &
+SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 20 -e 2016postVFP --userflags Skim1E2Mu,FakeStudy --python &
+SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 20 -e 2016postVFP --userflags Skim3Mu,FakeStudy --python &
+SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 30 -e 2017 --userflags Skim1E2Mu,FakeStudy --python &
+SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 30 -e 2017 --userflags Skim3Mu,FakeStudy --python &
+SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 40 -e 2018 --userflags Skim1E2Mu,FakeStudy --python &
+SKFlat.py -a ClosFakeRate -i TTLL_powheg --skim SkimTree_SS2lOR3l -n 40 -e 2018 --userflags Skim3Mu,FakeStudy --python &
