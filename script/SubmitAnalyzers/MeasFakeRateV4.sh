b7a03a2c2b1c657f21d86e06cee8236d7afe2b5f
Jin Choi <jin.choi@cern.ch>
2024-07-10 13:18:45 +0900

    make list of samples to avoid too many SKFlat.py call in script/SubmitAnalyzer/MeasFakeRateV4.sh

--- script/SubmitAnalyzers/MeasFakeRateV4.sh
+++ script/SubmitAnalyzers/MeasFakeRateV4.sh
@@ -16,21 +16,10 @@ if [[ $ERA == "2016"* ]]; then
         echo "Wrong channel $CHANNEL"
         exit 1
     fi
-    SKFlat.py -a MeasFakeRateV4 -i $DATASTREAM -n 10 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i TTLJ_powheg -n 30 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i TTLL_powheg -n 20 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i WJets_MG    -n 20 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i DYJets      -n 20 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i DYJets10to50_MG   -n 10 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i WW_pythia         -n 10 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i WZ_pythia         -n 10 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i ZZ_pythia         -n 10 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i SingleTop_sch_Lep -n 10 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i SingleTop_tch_top_Incl          -n 10 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i SingleTop_tch_antitop_Incl      -n 10 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i SingleTop_tW_top_NoFullyHad     -n 10 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i SingleTop_tW_antitop_NoFullyHad -n 10 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -l SampleLists/${QCD}.txt          -n 10 -e $ERA --userflags $CHANNEL,$FLAG &
+    SKFlat.py -a MeasFakeRateV4 -i $DATASTREAM               -n 10 -e $ERA --userflags $CHANNEL,$FLAG &
+    SKFlat.py -a MeasFakeRateV4 -l SampleLists/fakeHevay.txt -n 20 -e $ERA --userflags $CHANNEL,$FLAG &
+    SKFlat.py -a MeasFakeRateV4 -l SampleLists/fakeLight.txt -n 10 -e $ERA --userflags $CHANNEL,$FLAG &
+    SKFlat.py -a MeasFakeRateV4 -l SampleLists/${QCD}.txt    -n 10 -e $ERA --userflags $CHANNEL,$FLAG &
 elif [[ $ERA == "2017" ]]; then
     if [[ $CHANNEL == "MeasFakeMu"* ]]; then
         DATASTREAM="DoubleMuon"
@@ -42,21 +31,10 @@ elif [[ $ERA == "2017" ]]; then
         echo "Wrong channel $CHANNEL"
         exit 1
     fi
-    SKFlat.py -a MeasFakeRateV4 -i $DATASTREAM -n 10 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i TTLJ_powheg -n 30 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i TTLL_powheg -n 20 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i WJets_MG    -n 20 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i DYJets      -n 20 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i DYJets10to50_MG   -n 10 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i WW_pythia         -n 15 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i WZ_pythia         -n 15 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i ZZ_pythia         -n 15 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i SingleTop_sch_Lep -n 15 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i SingleTop_tch_top_Incl          -n 15 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i SingleTop_tch_antitop_Incl      -n 15 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i SingleTop_tW_top_NoFullyHad     -n 15 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i SingleTop_tW_antitop_NoFullyHad -n 15 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -l SampleLists/${QCD}.txt          -n 15 -e $ERA --userflags $CHANNEL,$FLAG &
+    SKFlat.py -a MeasFakeRateV4 -i $DATASTREAM               -n 10 -e $ERA --userflags $CHANNEL,$FLAG &
+    SKFlat.py -a MeasFakeRateV4 -l SampleLists/fakeHeavy.txt -n 20 -e $ERA --userflags $CHANNEL,$FLAG &
+    SKFlat.py -a MeasFakeRateV4 -l SampleLists/fakeLight.txt -n 15 -e $ERA --userflags $CHANNEL,$FLAG &
+    SKFlat.py -a MeasFakeRateV4 -l SampleLists/${QCD}.txt    -n 15 -e $ERA --userflags $CHANNEL,$FLAG &
 elif [[ $ERA == "2018" ]]; then
     if [[ $CHANNEL == "MeasFakeMu"* ]]; then
         DATASTREAM="DoubleMuon"
@@ -68,21 +46,10 @@ elif [[ $ERA == "2018" ]]; then
         echo "Wrong channel $CHANNEL"
         exit 1
     fi
-    SKFlat.py -a MeasFakeRateV4 -i $DATASTREAM -n 15 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i TTLJ_powheg -n 40 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i TTLL_powheg -n 30 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i WJets_MG    -n 30 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i DYJets      -n 30 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i DYJets10to50_MG   -n 20 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i WW_pythia         -n 20 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i WZ_pythia         -n 20 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i ZZ_pythia         -n 20 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i SingleTop_sch_Lep -n 20 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i SingleTop_tch_top_Incl          -n 20 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i SingleTop_tch_antitop_Incl      -n 20 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i SingleTop_tW_top_NoFullyHad     -n 20 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -i SingleTop_tW_antitop_NoFullyHad -n 20 -e $ERA --userflags $CHANNEL,$FLAG &
-    SKFlat.py -a MeasFakeRateV4 -l SampleLists/${QCD}.txt          -n 20 -e $ERA --userflags $CHANNEL,$FLAG &
+    SKFlat.py -a MeasFakeRateV4 -i $DATASTREAM               -n 15 -e $ERA --userflags $CHANNEL,$FLAG &
+    SKFlat.py -a MeasFakeRateV4 -l SampleLists/fakeHeavy.txt -n 30 -e $ERA --userflags $CHANNEL,$FLAG &
+    SKFlat.py -a MeasFakeRateV4 -l SampleLists/fakeLight.txt -n 20 -e $ERA --userflags $CHANNEL,$FLAG &
+    SKFlat.py -a MeasFakeRateV4 -l SampleLists/${QCD}.txt    -n 20 -e $ERA --userflags $CHANNEL,$FLAG &
 else
     echo "Wrong era $ERA"
     exit 1
